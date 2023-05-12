82523fb38e12e4664afd28537277d23b5af84353
Felix Garcia <fgarcia@tevolv.com>
2023-05-11 22:15:26 -0400

    feat: Spring boot Security implemented

---
+++ src/main/java/com/felix/market/web/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.felix.market.web.security;
+
+import com.felix.market.domain.service.FelixUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private FelixUserDetailsService felixUserDetailsService;
+
+    @Bean
+    public AuthenticationManager authManager (HttpSecurity security) throws Exception{
+        return security.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(felixUserDetailsService)
+                .and().build();
+
+    }
+}
